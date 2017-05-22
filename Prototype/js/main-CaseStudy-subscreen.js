51e5f91f68d7523363f05ec0555415ac03798123
Lenovo-E540\Mike Lee <st970703@gmail.com>
2017-05-22 22:58:35 +1200

    Finished most functionalities
    Finished most functionalities

--- Prototype/js/main-CaseStudy-subscreen.js
+++ Prototype/js/main-CaseStudy-subscreen.js
@@ -1,3 +1,14 @@
 function signOut() {
     confirm("Are you sure you want to sign out?");
 }
+
+function ansSubmitted() {
+    var ansTextArea = document.getElementById("ansTextArea").value = '';
+    alert("Your answer is submitted!");
+}
+
+function submitComment() {
+    var comment = document.getElementById("comment-input").value;
+    document.getElementById("comment-input").value = "";
+    $('#myNewComment').contents().first().replaceWith(comment);
+}
--- Prototype/js/main-CaseStudy.js
+++ Prototype/js/main-CaseStudy.js
@@ -1,3 +1,9 @@
 function signOut() {
     confirm("Are you sure you want to sign out?");
 }
+
+function search() {
+    var searchText = document.getElementById("searchText").value;
+    alert("Searching...\n" + searchText);
+    document.getElementById("searchText").value = "";
+}
