04e15d522bcb11f9faf46e9bf44b60d226f1b079
Lenovo-E540\Mike Lee <st970703@gmail.com>
2017-05-23 18:21:09 +1200

    single ans submission only
    single ans submission only

--- Prototype/js/main-CaseStudy-subscreen.js
+++ Prototype/js/main-CaseStudy-subscreen.js
@@ -2,13 +2,19 @@ function signOut() {
     confirm("Are you sure you want to sign out?");
 }
 
+submitted = false;
+
 function ansSubmitted() {
     var ansTextArea = document.getElementById("ansTextArea").value = '';
     alert("Your answer is submitted!");
+    submitted = true;
 }
 
 function submitComment() {
-    var comment = document.getElementById("comment-input").value;
-    document.getElementById("comment-input").value = "";
-    $('#myNewComment').contents().first().replaceWith(comment);
+    if (submitted == false) {
+        var comment = document.getElementById("comment-input").value;
+        document.getElementById("comment-input").value = "";
+        $('#myNewComment').contents().first().replaceWith(comment);
+        submitted = true;
+    }
 }
