8940c8d4b9548ae06fca4c622ee9f3e300db5e95
st970703 <st970703@users.noreply.github.com>
2017-07-25 01:09:08 +1200

    index.html
    index.html

---
+++ js/main-CaseStudy.js
@@ -0,0 +1,15 @@
+function signOut() {
+    confirm("Are you sure you want to sign out?");
+}
+
+function search() {
+    var searchText = document.getElementById("searchText ").value;
+    if (searchText !== "") {
+        alert("Searching...\n" + searchText);
+        document.getElementById("searchText ").value = "";
+    }
+}
+
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
